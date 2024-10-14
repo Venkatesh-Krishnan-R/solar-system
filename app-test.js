dc214b7a30a9dfcd4643d71aeb5f88b58fa49d46
Shaggyishere <157407663+Venkatesh-Krishnan-R@users.noreply.github.com>
2024-10-14 23:38:16 +0530

    Update app-test.js

--- app-test.js
+++ app-test.js
@@ -21,7 +21,7 @@ describe('Planets API Suite', () => {
               .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.have.property('id').eql(1);
-                    res.body.should.have.property('name').eql('Mercuryddd');
+                    res.body.should.have.property('name').eql('Mercury');
                 done();
               });
         });
@@ -51,7 +51,7 @@ describe('Planets API Suite', () => {
               .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.have.property('id').eql(3);
-                    res.body.should.have.property('name').eql('Earth');
+                    res.body.should.have.property('name').eql('Earthddd');
                 done();
               });
         });
